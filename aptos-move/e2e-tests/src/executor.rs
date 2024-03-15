892d3d6dadb58a3abc5549e4347404749c0327b6
Teng Zhang <rahxephon89@163.com>
2024-03-15 11:17:21 -0700

    fix

--- aptos-move/e2e-tests/src/executor.rs
+++ aptos-move/e2e-tests/src/executor.rs
@@ -45,8 +45,9 @@ use aptos_types::{
         signature_verified_transaction::{
             into_signature_verified_block, SignatureVerifiedTransaction,
         },
-        BlockOutput, ExecutionStatus, SignedTransaction, Transaction, TransactionOutput,
-        TransactionPayload, TransactionStatus, VMValidatorResult, ViewFunctionOutput,
+        BlockOutput, EntryFunction, ExecutionStatus, SignedTransaction, Transaction,
+        TransactionOutput, TransactionPayload, TransactionStatus, VMValidatorResult,
+        ViewFunctionOutput,
     },
     vm_status::VMStatus,
     write_set::{WriteOp, WriteSet, WriteSetMut},
@@ -663,6 +664,7 @@ impl FakeExecutor {
         &self,
         txn_block: Vec<Transaction>,
         state_view: &(impl StateView + Sync),
+        check_signature: bool,
     ) -> Result<Vec<TransactionOutput>, VMStatus> {
         let mut trace_map: (usize, Vec<usize>, Vec<usize>) = TraceSeqMapping::default();
 
@@ -677,7 +679,15 @@ impl FakeExecutor {
             }
         }
 
-        let sig_verified_block = into_signature_verified_block(txn_block);
+        let sig_verified_block = if check_signature {
+            into_signature_verified_block(txn_block)
+        } else {
+            let mut verified = vec![];
+            for txn in txn_block {
+                verified.push(SignatureVerifiedTransaction::Valid(txn))
+            }
+            verified
+        };
 
         let mode = self.executor_mode.unwrap_or_else(|| {
             if env::var(ENV_ENABLE_PARALLEL).is_ok() {
@@ -761,7 +771,7 @@ impl FakeExecutor {
         &self,
         txn_block: Vec<Transaction>,
     ) -> Result<Vec<TransactionOutput>, VMStatus> {
-        self.execute_transaction_block_with_state_view(txn_block, &self.data_store)
+        self.execute_transaction_block_with_state_view(txn_block, &self.data_store, true)
     }
 
     pub fn execute_transaction(&self, txn: SignedTransaction) -> TransactionOutput {
